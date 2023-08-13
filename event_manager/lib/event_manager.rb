088dd6ddd35a12f25a900dc4c15c8e4ace34ec7f
upkl <2737384+upkl@users.noreply.github.com>
2023-08-13 16:51:27 +0200

    Rubocop corrections

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,24 +1,21 @@
+# frozen_string_literal: true
+
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
 require 'date'
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
+  zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 def clean_phone_number(number)
-  if number.to_s.length == 10
+  case number.to_s.length
+  when 10
     number
-  elsif number.to_s.length == 11
-    if number[0] == '1'
-      number[1..]
-    else
-      nil
-    end
-  else
-    nil
-  end                             
+  when 11
+    number[1..] if number[0] == '1'
+  end
 end
 
 def legislators_by_zipcode(zip)
@@ -29,14 +26,14 @@ def legislators_by_zipcode(zip)
     civic_info.representative_info_by_address(
       address: zip,
       levels: 'country',
-      roles: ['legislatorUpperBody', 'legislatorLowerBody']
+      roles: %w[legislatorUpperBody legislatorLowerBody]
     ).officials
-  rescue
+  rescue StandardError
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
 
-def save_thank_you_letter(id,form_letter)
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('/tmp/output') unless Dir.exist?('/tmp/output')
 
   filename = "/tmp/output/thanks_#{id}.html"
@@ -68,21 +65,21 @@ contents.each do |row|
 
   form_letter = erb_template.result(binding)
 
-  save_thank_you_letter(id,form_letter)
+  save_thank_you_letter(id, form_letter)
 
   begin
     regdate = DateTime.parse(row[:regdate])
     h = regdate.hour
     hours[h] = hours.fetch(h, 0) + 1
-    
+
     wd = regdate.wday
     wday[wd] = wday.fetch(wd, 0) + 1
-  rescue
+  rescue StandardError
   end
 end
 
-hours_max = hours.max_by{ |hour, count| count}
-wday_max = wday.max_by{ |wday, count| count}
+hours_max = hours.max_by { |_hour, count| count }
+wday_max = wday.max_by { |_wday, count| count }
 
 puts "Peak hour: #{hours_max[0]}"
 puts "Peak wday: #{wday_max[0]}"
