3593f80daafcfa4b0c9d626baaa7da5ec7979d0a
upkl <2737384+upkl@users.noreply.github.com>
2023-08-13 17:05:24 +0200

    Rubucop-suggested changes

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 require 'json'
 
+# A hangman game
 class Game
   def initialize
-    dict = File.read('google-10000-english-no-swears.txt').split().filter { |w| 5 <= w.length and w.length <= 12 }
+    dict = File.read('google-10000-english-no-swears.txt').split.filter { |w| w.length >= 5 && w.length <= 12 }
 
     @word = dict.sample.upcase
 
@@ -12,20 +15,20 @@ class Game
   end
 
   def status
-    @word.split("").each { |c| print (@correct.include?(c) ? c : "_") }
+    @word.split('').each { |c| print(@correct.include?(c) ? c : '_') }
     puts
     puts
-    puts "Correct: #{@correct.sort.join('')}, Wrong: #{@incorrect.sort.join('')}, Left: #{@guesses_left}" 
+    puts "Correct: #{@correct.sort.join('')}, Wrong: #{@incorrect.sort.join('')}, Left: #{@guesses_left}"
   end
 
   def finished?
-    @word.split("").all? { |c| @correct.include?(c) }
+    @word.split('').all? { |c| @correct.include?(c) }
   end
 
   def save
-    print "Filename? "
+    print 'Filename? '
     fn = gets.chomp
-    f = File.open("save/" + fn, 'w')
+    f = File.open("save/#{fn}", 'w')
     f.write(JSON.dump({
                         word: @word,
                         guesses_left: @guesses_left,
@@ -36,31 +39,32 @@ class Game
   end
 
   def load
-    d = Dir.new("save")
-    files = d.to_a.reject { |f| f[0] == "." }
-    puts "Save files:"
+    d = Dir.new('save')
+    files = d.to_a.reject { |f| f[0] == '.' }
+    puts 'Save files:'
     files.each_with_index { |f, i| puts "#{i}) #{f}" }
-    print "Number? "
+    print 'Number? '
     n = gets.to_i
-    if 0 <= n and n < files.length
-      json_string = File.read("save/" + files[n])
-      values = JSON.parse(json_string, {symbolize_names: true})
-      @word = values[:word]
-      @guesses_left = values[:guesses_left]
-      @incorrect = values[:incorrect]
-      @correct = values[:correct]
-    end
+    return unless n >= 0 && n < files.length
+
+    json_string = File.read("save/#{files[n]}")
+    values = JSON.parse(json_string, { symbolize_names: true })
+    @word = values[:word]
+    @guesses_left = values[:guesses_left]
+    @incorrect = values[:incorrect]
+    @correct = values[:correct]
   end
-  
+
   def run
     loop do
       status
       print "Enter a letter: ('>' to save, '<' to load)"
       c = gets[0].upcase
-      if c == '>'
+      case c
+      when '>'
         save
         break
-      elsif c == '<'
+      when '<'
         load
         next
       else
@@ -74,7 +78,7 @@ class Game
           puts "You guessed #{@word}."
           break
         elsif @guesses_left <= 0
-          puts "You lost."
+          puts 'You lost.'
           break
         end
       end
@@ -83,4 +87,3 @@ class Game
 end
 
 Game.new.run
-
